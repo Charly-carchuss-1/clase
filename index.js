384af706865c2c19e1fb255b9d0439f0176f1e37
charly <c.a.r.c.h.u.s.s01@gmail.com>
2021-08-01 23:34:05 -0300

    Primer repo

---
+++ index.js
@@ -0,0 +1,35 @@
+
+
+console.log("Este es mi primer mensaje en Javascript");
+
+let nombre;
+let email;
+let localidad;
+let comentario;
+
+
+function guardarDatos() {
+    nombre = document.getElementById("nombre").value;
+    email = document.getElementById("email").value;
+    localidad = document.getElementById("localidad").value;
+    comentario = document.getElementById("comentario").value;
+
+    console.log(nombre);
+    console.log(email);
+    console.log(localidad);
+    console.log(comentario);
+}
+
+
+function suma(a, b){
+    console.log( a + b);
+}
+
+
+
+
+
+console.log(nombre);
+
+suma(10, 20);
+
